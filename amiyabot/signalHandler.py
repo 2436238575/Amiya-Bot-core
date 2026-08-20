9bd7055dd641ea1ae7e04093351c32bb8c4bcb8d
熵 <2436238575@qq.com>
2026-08-20 18:34:16 +0800

    feat: 退出回调

--- amiyabot/signalHandler.py
+++ amiyabot/signalHandler.py
@@ -7,19 +7,25 @@ from typing import List, Callable
 
 class SignalHandler:
     on_shutdown: List[Callable] = []
+    shutdown_event = asyncio.Event()
+    shutdown_tasks: List[asyncio.Task] = []
 
     @classmethod
     def exec_shutdown_handlers(cls):
+        cls.shutdown_tasks.clear()
         for action in cls.on_shutdown:
             if inspect.iscoroutinefunction(action):
-                asyncio.create_task(action())
+                cls.shutdown_tasks.append(asyncio.create_task(action()))
             else:
                 action()
 
 
 def sigint_handler(*args):
+    if getattr(sigint_handler, '_handled', False):
+        return
+    sigint_handler._handled = True
     SignalHandler.exec_shutdown_handlers()
-    # sys.exit(0)
+    SignalHandler.shutdown_event.set()
 
 
 signal.signal(signal.SIGINT, sigint_handler)
